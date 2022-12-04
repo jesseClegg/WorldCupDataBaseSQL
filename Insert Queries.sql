dd7f47a3679fb33496934bd72744f1db6d3cc98e
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-12-03 19:09:28 -0500

    add queries for country table

---
+++ Insert Queries.sql
@@ -0,0 +1,13 @@
+INSERT INTO Country 
+VALUES ("England", 55000000)
+
+
+
+INSERT INTO 
+	Country(Name, Population)
+VALUES
+	("United States", 310000000),
+	("France", 67000000),
+	("England", 55000000),
+	("Germany", 83000000),
+	("Italy", 59000000);
