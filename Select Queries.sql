5cd9a04306721089dbf82a8d9b907561c4ce0da3
jesseClegg <89627047+jesseClegg@users.noreply.github.com>
2022-12-05 22:28:10 -0500

    formatted nicely, only need question 3 now

--- Select Queries.sql
+++ Select Queries.sql
@@ -1,4 +1,4 @@
-Question 1
+########Question 1
 	Select M.name
     FROM Member M
     Where M.memberId In
@@ -13,8 +13,7 @@ Question 1
 			Where T.Year="2018" && T.Country ="Australia"
 			)));
 
-Question 2
-
+########Question 2
 	SELECT Distinct Name
 	FROM Member M
     Where M.MemberId in(
@@ -22,19 +21,12 @@ Question 2
     From Goals G
     Where G.Count < 1);
 
+########Question 3
+Select Distinct Tm.teamId
+From team Tm, Tournament T
+Where Tm.country=T.country ;
 
-Question 3
-
-NOTE DONE
- Select Distinct *
-From `Match` M
-Where M.MatchId In (
-Select MatchId
-From PoolGame
-);
-
-
-Question 4
+########Question 4
 SELECT PlayerID 
 FROM Player WHERE HomeClubName IN
 	(SELECT HomeClubName 
@@ -48,22 +40,54 @@ AND PlayerId IN
 		FROM TEAM 
 		WHERE Country='Australia'));
 
+########Question 5
+Select TeamId
+from TeamMember
+Where TeamMember.MemberId IN 
+	(Select staffId 
+	From supportstaff)
+AND TeamMember.Year="2018"
+Group By TeamMember.TeamId
+Order By count(*) DESC;
 
+########Question 6
+Select M.Name, Sum(count)
+From Saves S, Member M
+Where PlayerId in(
+	Select PlayerId 
+	From Player) And S.PlayerId=M.MemberId
+    Group by PlayerId;
 
-
-Question 7
+########Question 7
 SELECT max(cnt)
 FROM
 	(Select count(*) as cnt
 	FROM ticket T
 	Group by T.matchId) c
 
+########Question 8
+Select Member.Name
+From Member
+Where Member.MemberId In(
+Select Distinct PlayerId 
+From Goals
+Where Goals.PlayerId In (
+	Select  GP.PlayerId
+	From Goals GP
+	Where GP.MatchId IN(
+		Select E.EliminationId
+		From EliminationGame E)	 
+	Group By PlayerId
+	Having Sum(count) >= ALL 
+		(Select Sum(count)
+		From Goals GP
+		Where GP.MatchId IN
+			(Select E.EliminationId
+			From EliminationGame E)	 
+			Group By PlayerId
+    )));
 
-
-
-
-Question 9
-
+########Question 9
 SELECT  Country
 From Team
 Group by Country
@@ -71,81 +95,7 @@ Having count(Country)>=(
 SELECT Count(Year)
 From Tournament);
 
-Question 10
-/////////////////// OLD
-Select MatchId 
-From `Match` M
-Where M.HomeTeamId in (
-Select TeamId
-From Team 
-Where Team.Country = "Iceland"
-)
-OR 
-M.AwayTeamId in (
-Select TeamId
-From Team 
-Where Team.Country = "Iceland"
-);
-
-
-//////////////// OLD
-Select MatchID 
-From Goals 
-Where Goals.MatchId IN(
-	Select MatchId 
-	From `Match` M
-	Where M.HomeTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-	OR 
-	M.AwayTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-)
-;
-////// latest version below ////
-
-
-Select PlayerId, count(*)
-From Goals 
-Where Goals.MatchId IN(
-	Select MatchId 
-	From `Match` M
-	Where M.HomeTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-	OR 
-	M.AwayTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-)
-Group by PlayerId
-
-
-/////
-getting how many games iceland played:
-Select Count(*)
-From Goals 
-Where Goals.MatchId IN(
-	Select MatchId 
-	From `Match` M
-	Where M.HomeTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-	OR 
-	M.AwayTeamId in 
-		(Select TeamId
-		From Team 
-		Where Team.Country = "Iceland")
-)
-
-//////////////////////////////[10]
-//this should be the good one!!! PROVIDED WE REMOVE ZERO VALUES FROM SCORE!
+########Question 10
 Select PlayerId, count(*)
 From Goals 
 Where Goals.MatchId IN(
@@ -180,192 +130,18 @@ Where Goals.MatchId IN(
 
 
 
-//////////// question 6
-
-
-Select M.Name, Sum(count)
-From Saves S, Member M
-Where PlayerId in(
-	Select PlayerId 
-	From Player) And S.PlayerId=M.MemberId
-    Group by PlayerId;
 
     
 
 
-/////////// number 8
-
-Select M.Name, Sum(count) 
-From Goals GP, Member M 
-Where GP.MatchId IN(
-	Select MatchID
-	From Goals G
-	Where G.MatchId In(
-		Select E.EliminationId
-		From EliminationGame E
-)	) 
-AND M.MemberId=GP.PlayerId
-Group By PlayerId;
-
-
-
-
-
-
-///// after zooom call: SelectSelect Sum(count) 
-From Goals GP, Member M 
-Where GP.MatchId IN(
-	Select MatchID
-	From Goals G
-	Where G.MatchId In(
-		Select E.EliminationId
-		From EliminationGame E
-)	) 
-AND M.MemberId=GP.PlayerId
-Group By PlayerId
-Having Sum(count)>= All (
-	Select Sum(count) 
-	From Goals GP, Member M 
-	Where GP.MatchId IN(
-		Select  
-		From Goals G
-		Where G.MatchId In(
-			Select E.EliminationId
-			From EliminationGame E
-)	) 
-AND M.MemberId=GP.PlayerId
-Group By PlayerId);
-
-
-////// only slightly off now
-Select M.Name, Sum(count)
-From Goals GP, Member M 
-Where GP.MatchId IN(
-	Select MatchID
-	From Goals G
-	Where G.MatchId In(
-		Select E.EliminationId
-		From EliminationGame E
-)	) 
-AND M.MemberId=GP.PlayerId
-Group by M.name
-Having Sum(count) >= ANY (
-
--- Select Sum(count) 
--- From Goals GP, Member M 
--- Where GP.MatchId IN(
--- 	Select MatchID
--- 	From Goals G
--- 	Where G.MatchId In(
--- 		Select E.EliminationId
--- 		From EliminationGame E
--- )	) 
--- AND M.MemberId=GP.PlayerId
--- Group By PlayerId)
--- ;
 
 	
 
 
-DO NOT LOSE THIS!!
-
-Select Sum(count), M.MemberId 
-From Goals GP, Member M 
-Where GP.MatchId IN(
-		Select E.EliminationId
-		From EliminationGame E
-)	 
-AND M.MemberId=GP.PlayerId
-Group By PlayerId
-;
-
-
-	
-///pretty close, kanye is a problem
-Select Mo.Name, Sum(count)
-From Goals Go, Member Mo 
-Where Go.MatchId IN(
-		Select Eo.EliminationId
-		From EliminationGame Eo
-)	 
-AND Mo.MemberId=Go.PlayerId
-Group by Mo.name
-
-Having Sum(count) >= ANY (
-	Select Sum(count)
-	From Goals GP, Member M 
-	Where GP.MatchId IN(
-		Select E.EliminationId
-		From EliminationGame E
-	)	 
-AND M.MemberId=GP.PlayerId
-Group By PlayerId
-);
-
-	
-
-
-//this is actually it!!
-
-	Select Sum(count), GP.PlayerId
-	From Goals GP
-	Where GP.MatchId IN(
-		Select E.EliminationId
-		From EliminationGame E)	 
-	Group By PlayerId
-	Having Sum(count) >= ANY 
-		(Select Sum(count)
-		From Goals GP
-		Where GP.MatchId IN
-			(Select E.EliminationId
-			From EliminationGame E)	 
-			Group By PlayerId
-    );
-	
 
 
 
 
-///final solution
-Select Member.Name
-From Member
-Where Member.MemberId In(
-Select Distinct PlayerId 
-From Goals
-Where Goals.PlayerId In (
-	Select  GP.PlayerId
-	From Goals GP
-	Where GP.MatchId IN(
-		Select E.EliminationId
-		From EliminationGame E)	 
-	Group By PlayerId
-	Having Sum(count) >= ALL 
-		(Select Sum(count)
-		From Goals GP
-		Where GP.MatchId IN
-			(Select E.EliminationId
-			From EliminationGame E)	 
-			Group By PlayerId
-    )));
-	
-
-
-////question 5
-Select TeamId
-from TeamMember
-Where TeamMember.MemberId IN 
-	(Select staffId 
-	From supportstaff)
-AND TeamMember.Year="2018"
-Group By TeamMember.TeamId
-Order By count(*) DESC;
-
-
-/// Question 3
-
-Select Distinct Tm.teamId
-From team Tm, Tournament T
-Where Tm.country=T.country ;
 
 
 
